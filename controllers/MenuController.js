b25a1ea1bf5fcfbdef2c26ca28f54d7dddcc42d8
Jared Moats <JaredMoats@outlook.com>
2019-02-26 23:20:18 -0500

    Checkpoint 4 listing contacts and searching: complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,7 +10,9 @@ module.exports = class MenuController {
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
-          "Date",
+          "View all contacts",
+          "Search for a contact",
+          "Get today's date",
           "Exit"
         ]
       }
@@ -35,6 +37,23 @@ module.exports = class MenuController {
     console.log("\x1Bc");
   }
 
+  delete(contact) {
+    inquirer.prompt(this.book.deleteConfirmQuestions)
+    .then(answer => {
+      if(answer.confirmation) {
+        this.book.delete(contact.id);
+        console.log("Contact deleted!");
+        this.main();
+      } else {
+        console.log("Contact not deleted");
+        this.showContact(contact);
+      }
+    }).catch(error => {
+      console.log(error);
+      this.main();
+    });
+  }
+
   exit() {
     console.log("Thanks for using AddressBloc!");
     process.exit();
@@ -42,6 +61,25 @@ module.exports = class MenuController {
 
   getContactCount() {
     return this.contacts.length;
+  }
+
+  getContacts() {
+    this.clear();
+
+    this.book.getContacts().then(contacts => {
+      for(let contact of contacts) {
+        console.log(`
+          name: ${contact.name}
+          phone number: ${contact.phone}
+          email: ${contact.email}
+          --------------
+        `);
+      }
+      this.main();
+    }).catch(error => {
+      console.log(error);
+      this.main();
+    });
   }
 
   getDate() {
@@ -63,7 +101,13 @@ module.exports = class MenuController {
         case "Add new contact":
           this.addContact();
           break;
-        case "Date":
+        case "View all contacts":
+          this.getContacts();
+          break;
+        case "Search for a contact":
+          this.search();
+          break;
+        case "Get today's date":
           this.getDate();
           break;
         case "Exit":
@@ -81,4 +125,56 @@ module.exports = class MenuController {
   remindMe() {
     return "Learning is a life-long pursuit";
   }
+
+  search(){
+      inquirer.prompt(this.book.searchQuestions)
+      .then((target) => {
+       this.book.search(target.name)
+       .then((contact) => {
+          if(contact === null){
+            this.clear();
+            console.log("contact not found");
+            this.search();
+          } else {
+            this.showContact(contact);
+         }
+
+        });
+     })
+     .catch((err) => {
+       console.log(err);
+       this.main();
+     });
+    }
+
+    showContact(contact){
+      this._printContact(contact);
+      inquirer.prompt(this.book.showContactQuestions)
+      .then(answer => {
+        switch(answer.selected) {
+          case "Delete contact":
+            this.delete(contact);
+            break;
+          case "Main menu":
+            this.main();
+            break;
+          default:
+            console.log("Something went wrong.");
+            this.showContact(contact);
+        }
+      }).catch(error => {
+        console.log(error);
+        this.showContact(contact);
+      })
+    }
+
+    _printContact(contact){
+      console.log(`
+        name: ${contact.name}
+        phone number: ${contact.phone}
+        email: ${contact.email}
+        ---------------`
+      );
+    }
+
 }//end of class
